abf9c9a0ec91af5ca092b3d5c4210c57852251c2
egeominotti <egeominotti@gmail.com>
2022-08-03 18:55:39 +0200

    - added binance api - added kda price

--- runner.js
+++ runner.js
@@ -2,6 +2,10 @@ const axios = require('axios').default;
 const schedule = require('node-schedule');
 const moment = require('moment')
 const express = require('express')
+const Binance = require('node-binance-api');
+
+const binance = new Binance().options({});
+
 const app = express()
 
 require('dotenv').config();
@@ -23,20 +27,28 @@ async function main() {
 
     try {
 
+        let ticker = await binance.prices();
         let res_kd = await axios.get(process.env.URL_API_KADDEX + "dateStart=" + current_date + "&dateEnd=" + current_date)
         let res_dl = await axios.get(process.env.URL_API_DEFILAMA)
 
 
+        console.log(ticker.KDAUSDT)
         if (res_kd.status === 200) {
             price = res_kd.data[0].usdPrice.close.toFixed(3);
         }
 
         if (res_dl.status === 200) {
-            tvl = res_dl.data.currentChainTvls.Kadena.toFixed(0);
+            tvl = parseFloat(res_dl.data.currentChainTvls.Kadena).toFixed(0);
         }
 
-        let text = 'Bot Staking Kaddex\n \nCurrent TVL: ' + tvl + "\nCurrent PRICE: " + price + " \n" + "\nBy kernelvoid";
-        await axios.get(telegram + text)
+        let txt = 'Bot Staking Kaddex\n ' +
+            '\nKDA Price: $' + parseFloat(ticker.KDAUSDT).toFixed(2) + ' ' +
+            "\nKDX Price: $" + price + " " +
+            '\nCurrent TVL: $' + tvl + "" +
+            "\n" + "" +
+            "\nBy kernelvoid";
+
+        await axios.get(telegram + txt)
 
     } catch (e) {
         await axios.get(telegram + e.toString())
