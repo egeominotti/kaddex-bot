c5e374dc200105f61b056e4d5f338904f4d63abc
egeominotti <egeominotti@gmail.com>
2022-08-06 05:47:22 +0200

    - increase time

--- runner.js
+++ runner.js
@@ -38,27 +38,23 @@ async function main() {
 
     console.log("I'm alive!!");
 
-    const current_date = moment().format("Y-MM-DD")
+    //const current_date = moment().format("Y-MM-DD")
 
     let tvl = 0;
-    let price = 0
+    //let price = 0
 
     try {
 
-        const browser = await puppeteer.launch({
-            headless: true,
-            slowMo: 500,
-            args: ['--no-sandbox']
-        });
-
-        let res_kd = await axios.get(process.env.URL_API_KADDEX + "dateStart=" + current_date + "&dateEnd=" + current_date)
+        //let res_kd = await axios.get(process.env.URL_API_KADDEX + "dateStart=" + current_date + "&dateEnd=" + current_date)
         let res_dl = await axios.get(process.env.URL_API_DEFILAMA)
 
+        /*
         if (res_kd.status === 200) {
             if (res_kd.data[0].usdPrice !== undefined) {
                 price = res_kd.data[0].usdPrice.close.toFixed(3);
             }
         }
+         */
 
         if (res_dl.status === 200) {
             if (res_dl.data.currentChainTvls !== undefined) {
@@ -67,57 +63,57 @@ async function main() {
         }
 
 
-        if (tvl !== tvl_stored || price !== price_stored) {
+        //if (tvl !== tvl_stored || price !== price_stored) {
 
-            const page = await browser.newPage();
+        const browser = await puppeteer.launch({
+            headless: true,
+            slowMo: 500,
+            args: ['--no-sandbox']
+        });
 
-            await page.goto(URL_API_KADDEX_STATS, {waitUntil: 'networkidle2'});
-            const element = await page.waitForSelector('.FlexContainer__STYFlexContainer-sc-16sly3k-0.flviZN.column');
-            const value = await element.evaluate(el => el.textContent);
+        const page = await browser.newPage();
 
-            console.log(value.split(" "))
+        await page.goto(URL_API_KADDEX_STATS, {waitUntil: 'networkidle2'});
+        const element = await page.waitForSelector('.FlexContainer__STYFlexContainer-sc-16sly3k-0.flviZN.column');
+        const value = await element.evaluate(el => el.textContent);
 
-            const value_splitted = value.split(" ");
+        console.log(value.split(" "))
 
-            let percentage = '';
+        const value_splitted = value.split(" ");
 
-            if (value_splitted[1].includes('+')) percentage = 'incremento';
-            if (value_splitted[1].includes('-')) percentage = 'decremento';
+        let percentage = '';
 
-            const value_kdx = value_splitted[1] + " " + percentage + " " + value_splitted[2]
-            const market_cap = value_splitted[5].replace('supply', '').replace('-', '').replace(' ', '')
-            const circulating_supply = value_splitted[8].replace('supply', ' ').replace(' ', '')
-            const burned = value_splitted[10].replace('%Burned', ' ').replace(' ', '')
+        if (value_splitted[1].includes('+')) percentage = 'incremento';
+        if (value_splitted[1].includes('-')) percentage = 'decremento';
 
-            console.log("Value kdx: " + value_kdx)
-            console.log("Market cap: " + market_cap)
-            console.log("Circulating supply: " + circulating_supply)
-            console.log("Burned: " + burned)
-            console.log("TVL: " + tvl)
-            console.log("TVL Stored: " + tvl_stored)
-            console.log("Price KDX: " + price)
-            console.log("Price Stored: " + price_stored)
+        const value_kdx = value_splitted[1] + " " + percentage + " " + value_splitted[2]
+        const market_cap = value_splitted[5].replace('supply', '').replace('-', '').replace(' ', '')
+        const circulating_supply = value_splitted[8].replace('supply', ' ').replace(' ', '')
+        const burned = value_splitted[10].replace('%Burned', ' ').replace(' ', '')
 
-            let ticker = await binance.prices();
+        console.log("Value kdx: " + value_kdx)
+        console.log("Market cap: " + market_cap)
+        console.log("Circulating supply: " + circulating_supply)
+        console.log("Burned: " + burned)
+        console.log("TVL Stored: " + tvl_stored)
+        console.log("Price Stored: " + price_stored)
 
-            tvl_stored = tvl;
-            price_stored = price;
+        let ticker = await binance.prices();
 
+        let txt = '-- KADDEX BOT --\n ' +
+            '\nKDA Price: ' + parseFloat(ticker.KDAUSDT).toFixed(3) + '$' +
+            "\nKDX Price: " + String(value_kdx) + "%" +
+            '\nCurrent TVL: ' + formatNumber(tvl) + "$" +
+            '\nMarket Cap: ' + market_cap + "$" +
+            '\nCirculating supply: ' + circulating_supply + "$" +
+            '\nBurned: ' + burned + "$" +
+            "\n" + "" +
+            "\nValue Updated: " + "" + moment().format("Y-MM-DD h:mm:ss") + " \n" +
+            "\n" + ""
 
-            let txt = '-- KADDEX BOT --\n ' +
-                '\nKDA Price: ' + parseFloat(ticker.KDAUSDT).toFixed(3) + '$' +
-                "\nKDX Price: " + String(value_kdx) + "%" +
-                '\nCurrent TVL: ' + formatNumber(tvl) + "$" +
-                '\nMarket Cap: ' + market_cap + "$" +
-                '\nCirculating supply: ' + circulating_supply + "$" +
-                '\nBurned: ' + burned + "$" +
-                "\n" + "" +
-                "\nValue Updated: " + "" + moment().format("Y-MM-DD h:mm:ss") + " \n" +
-                "\n" + ""
-
-            await axios.get(telegram +  txt)
-            await browser.close();
-        }
+        await axios.get(telegram + txt)
+        await browser.close();
+        //}
 
     } catch (e) {
         await axios.get(telegram + e.toString())
@@ -127,6 +123,3 @@ async function main() {
 schedule.scheduleJob('*/30 * * * *', async function () {
     await main();
 });
-
-
-//setInterval(main, 50000);
